57dfddc76e13ccd60c4d60aac1974e6f3a7bcec3
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-07-18 22:07:12 -0300

    jest health

--- src/app.js
+++ src/app.js
@@ -3,15 +3,8 @@ import api from "./api/index.js";
 import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT;
 
 app.use("", api)
 app.use(json());
 
-app.listen(PORT, (error) => {
-    if (!error)
-        console.log("Server is Successfully Running, and App is listening on port " + PORT)
-    else
-        console.log("Error occurred, server can't start", error);
-}
-);
+export default app

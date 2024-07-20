ab361432a32ed750c151cbf4ea352855fe3a47cc
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-07-20 10:07:12 -0300

    refactored tests

--- src/api/appointments/controller.test.js
+++ src/api/appointments/controller.test.js
@@ -1,8 +1,21 @@
 import request from "../../utils/tests/custom_request"
-import { appointment_1 } from "../../utils/tests/objectMothers/appointmentsMother.js"
+import {
+    appointment_1,
+    transformAppointmentToDTO,
+} from "../../utils/tests/objectMothers/appointmentsMother.js"
 import { doctor_1 } from "../../utils/tests/objectMothers/doctorsMother"
 import { patient_1 } from "../../utils/tests/objectMothers/patientsMother"
 import appointmentSchema from "./model.js"
+
+function copyDocumentWithout(document, ...args) {
+    const copy = { ...document }
+
+    for (let arg of args) {
+        delete copy[arg]
+    }
+
+    return copy
+}
 
 describe("PATCH /approval-status", () => {
     it("Unauthorized to patient", async () => {
@@ -23,20 +36,14 @@ describe("PATCH /approval-status", () => {
 
 describe("POST /", () => {
     it("Authorized to patient", async () => {
-        const payload = {
-            doctorCRM: appointment_1.doctor.crm,
-            appointmentStart: appointment_1.appointmentStart,
-        }
+        const appointmentDTO = transformAppointmentToDTO(appointment_1)
 
         await request(appointment_1.patient)
             .post("/appointments")
-            .send(payload)
+            .send(copyDocumentWithout(appointmentDTO, "patientCPF"))
             .expect(200)
 
-        await expect(appointmentSchema).hasOneDocumentWith({
-            patientCPF: appointment_1.patient.cpf,
-            ...payload,
-        })
+        await expect(appointmentSchema).hasOneDocumentWith(appointmentDTO)
     })
     it("Unauthorized to doctor", async () => {
         await request(doctor_1).post("/appointments").send({}).expect(403)
--- src/utils/tests/objectMothers/appointmentsMother.js
+++ src/utils/tests/objectMothers/appointmentsMother.js
@@ -1,8 +1,16 @@
 import { doctor_1 } from "./doctorsMother"
 import { patient_1 } from "./patientsMother"
 
+export const transformAppointmentToDTO = (appointment) => {
+    return {
+        doctorCRM: appointment.doctor.crm,
+        appointmentStart: appointment.appointmentStart,
+        patientCPF: appointment.patient.cpf,
+    }
+}
+
 export const appointment_1 = {
     doctor: doctor_1,
     patient: patient_1,
-    appointmentStart: "2015-03-25T12:00:00.000Z"
+    appointmentStart: "2015-03-25T12:00:00.000Z",
 }

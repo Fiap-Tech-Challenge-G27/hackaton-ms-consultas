f17d24b56660e4ffb9ae89fe86adc7a2779eac5d
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-07-26 15:55:32 -0300

    test enviroment

--- src/config.js
+++ src/config.js
@@ -1,19 +1,26 @@
 import "dotenv/config"
 
 const requireProcessEnv = (name) => {
-    if (!process.env[name]) {
+    if (!process.env[name] && process.env.NODE_ENV !== "test") {
         throw new Error("You must set the " + name + " environment variable")
     }
     return process.env[name]
 }
 
-export default {
-    port: process.env.PORT || 8080,
-    jwtSecret: requireProcessEnv("JWT_SECRET"),
-    mongo: {
-        uri: requireProcessEnv("MONGODB_URI"),
-        options: {
-            debug: false,
+const config = {
+    all: {
+        env: process.env.NODE_ENV || "development",
+        port: process.env.PORT || 8080,
+        jwtSecret: requireProcessEnv("JWT_SECRET"),
+        mongo: {
+            uri: requireProcessEnv("MONGODB_URI"),
+            options: {
+                debug: false,
+            },
         },
     },
+    test: {
+        jwtSecret: "jwt secret for tests",
+    },
 }
+export default { ...config.all, ...config[config.all.env] }
